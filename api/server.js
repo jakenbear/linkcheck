3a6e1e19bfddae38e150c8bc060658e40d874670
Jake Reardon <jakenbear@gmail.com>
2025-12-10 16:57:35 -0500

    Add explicit route for root to serve index.html

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.post('/check', async (req, res) => {
   const { links } = req.body;
